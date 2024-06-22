3da882d7ca7fa4102f447222ff11cefa6ef07c71
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-22 15:45:50 -0300

    Statusline config

--- lua/mateus/plugins/statusline.lua
+++ lua/mateus/plugins/statusline.lua
@@ -1,23 +1,69 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    local default_theme = { fg = "#ebbcba", bg = "None" }
+
+    lualine.setup({
       sections = {
-        lualine_c = {
-          {
-            "filename",
-            file_status = true,
-            path = 4,
-          },
-        },
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = { "branch", "diff", "diagnostics" },
       },
       options = {
+        theme = {
+          normal = {
+            a = default_theme,
+            b = default_theme,
+            c = default_theme,
+            y = default_theme,
+            x = default_theme,
+          },
+          inactive = {
+            a = default_theme,
+            b = default_theme,
+            c = default_theme,
+            y = default_theme,
+            x = default_theme,
+          },
+        },
         globalstatus = true,
         component_separators = "",
         section_separators = "",
       },
-    },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+    })
+
+    vim.opt.fillchars = {
+      stl = "─",
+      stlnc = "─",
+    }
+  end,
 }
+--
+-- return {
+--   {
+--     "nvim-lualine/lualine.nvim",
+--     event = "VeryLazy",
+--     opts = {
+--       sections = {
+--         lualine_c = {
+--           {
+--             "filename",
+--             file_status = true,
+--             path = 4,
+--           },
+--         },
+--       },
+--       options = {
+--         globalstatus = true,
+--         component_separators = "",
+--         section_separators = "",
+--       },
+--     },
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--   },
+-- }
