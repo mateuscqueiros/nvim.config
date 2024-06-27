e2d6ffa99a8f9199a145e04293eb21b37c934257
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-27 12:34:08 -0300

    rip-substitute

---
+++ lua/mateus/plugins/rip-substitute.lua
@@ -0,0 +1,13 @@
+return {
+  "chrisgrieser/nvim-rip-substitute",
+  keys = {
+    {
+      "<leader>fs",
+      function()
+        require("rip-substitute").sub()
+      end,
+      mode = { "n", "x" },
+      desc = " rip substitute",
+    },
+  },
+}
